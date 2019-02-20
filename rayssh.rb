d784bb1871a2928b8cedd2a8e97610fc617ec726
vsberm <mlm@vsber.com>
2019-02-20 13:27:59 +0800

    first commit

---
+++ rayssh.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Rayssh < Formula
+  desc "rayssh transfer"
+  homepage "https://www.raysync.cn/"
+  url "https://raw.githubusercontent.com/vsberm/rayssh-release/master/macOS/rayssh-0.0.1.tar.gz"
+  sha256 "62a295ea7e88286ecbfe13b1eb30ba7db298e87c3716f64346bb8f417aee21b7"
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # system "cmake", ".", *std_cmake_args
+    bin.install "./bin/rayscp"
+    bin.install "./bin/rayssh"
+    bin.install "./bin/rayssh-keygen"
+    bin.install "./bin/rsync"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test rayssh`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
